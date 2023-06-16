ad6de10114b379fb886ec2d2dc59052b0c28fb90
12103197 <ajaymanchala1208@gmail.com>
2023-06-16 13:27:04 +0530

    Intro SuperStudy.java

---
+++ SuperStudy.java
@@ -0,0 +1,34 @@
+package Keywords;
+class Animal {
+
+	  // overridden method
+	  public void display(){
+	    System.out.println("I am an animal");
+	  }
+	}
+
+	class Dog extends Animal {
+
+	  // overriding method
+	  @Override
+	  public void display(){
+	    System.out.println("I am a dog");
+	  }
+
+	  public void printMessage(){
+
+	    // this calls overriding method
+	    display();
+
+	    // this calls overridden method
+	    super.display();
+	  }
+	}
+
+public class SuperStudy {
+	public static void main(String[] args) {
+	    Dog dog1 = new Dog();
+	    dog1.printMessage();
+	  }
+
+}
